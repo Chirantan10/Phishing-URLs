998fac5f05838776fa98abdf519aca8012a168a2
CHIRANTAN NATH <154256163+Chirantan10@users.noreply.github.com>
2024-12-25 18:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def predict():
 if __name__ == '__main__':
     # Get the port from the environment variable or use 5000 as a fallback
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
